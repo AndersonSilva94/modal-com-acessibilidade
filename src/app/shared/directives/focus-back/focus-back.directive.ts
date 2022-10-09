a6aaea319ca5a53111349fef98dd343ddae695df
Anderson Silva <andssilva.23@gmail.com>
2022-10-09 01:01:14 -0400

    feat: :rocket: criando a diretiva para dar foco ao último elemento clicado antes de abrir a modal

---
+++ src/app/shared/directives/focus-back/focus-back.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, OnDestroy, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appFocusBack]'
+})
+export class FocusBackDirective implements OnInit, OnDestroy {
+
+  private lastFocusableElement: Element;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.lastFocusableElement = document.activeElement
+  }
+
+  ngOnDestroy(): void {
+    if(this.lastFocusableElement) {
+      (this.lastFocusableElement as HTMLElement).focus();
+    }
+  }
+
+}
