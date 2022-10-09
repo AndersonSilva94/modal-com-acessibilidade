5015c26e96e615e8f12a3269c8332bf24a3ab6eb
Anderson Silva <andssilva.23@gmail.com>
2022-10-08 21:07:56 -0400

    feat: :rocket: adicionando fadeIn animation

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { fade } from '../../animations/fade';
 import { ModalConfig } from './interfaces/modal-config';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
+  animations: [fade]
 })
 export class ModalComponent implements OnInit {
+  @HostBinding('@fade') fade = true;
 
   public config!: ModalConfig;
 
